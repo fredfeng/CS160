61737ba1970419abf0d1cf0fc91be9ebb3bc753c
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2023-04-06 13:29:32 -0700

    Add notes for section 1
    Add section 1 notes

---
+++ sections/sec01/ex1.ml
@@ -0,0 +1,48 @@
+(* 'a is a "type variable" - which means it's a "placeholder" for a type, that can be substituted in function application *)
+(* what is the type of (id 3)? What is the type of (id "hello world")?  How do we figure that out? *)
+let id (x : 'a) : 'a = x;;
+
+(* recursive data type - a list *)
+(* You haven't learned ADTs yet in lecture, but think of this as a "preview" for the next lecture *)
+type 'a mylist = MNil | MCons of 'a * 'a mylist;; (* type variables are placed in front in Ocaml *)
+let example_list : int mylist = (MCons (3 ,(MCons (1, (MCons (2, MNil))))));;
+
+(* length function *)
+let rec my_len: 'a mylist -> int = function
+  | MNil -> 0
+  | MCons (h,t) -> 1 + my_len t;;
+
+Printf.printf "Length of example_list is: %d\n" (my_len example_list);;
+
+(* buggy - actual bug as I was writing this. Type signature will give away the bug.. *)
+(* use trace to fix! *)
+let rec buggy_my_len = function
+  | MNil -> 0
+  | MCons (h,t) -> h + buggy_my_len t;;
+
+Printf.printf "Length of example_list with buggy function is: %d\n" (buggy_my_len example_list);;
+
+(* map - given a function and a list, return a new list with the function to applied to all elements of the given list *)
+
+(* if we have time, try to define these. Otherwise, I'll just give them. *)
+(* another way to write functions *)
+let rec my_map (f: ('a -> 'b)) (l: 'a mylist) : 'b mylist =
+  match l with
+  | MNil -> MNil
+  | MCons (h, t) -> MCons (f h, my_map f t)
+(* example usage: my_map (fun x -> x + 3) example_list;; *)
+
+(* map + fold *)
+
+(* right associative fold - simpler, but more restrictive, type *)
+let rec my_fold (f: ('a -> 'a -> 'a)) (i: 'a) (l: 'a mylist ): 'a =
+  match l with
+  | MNil -> i (* when list is nil*)
+  | MCons (h, t) -> f h (my_fold f i t)
+(* example usage: my_fold (fun a b -> a + b) 0 example_list;; *)
+
+(* composition! Count elements in a list of lists! *)
+let example_list_of_lists = (MCons (example_list, MCons (example_list, MNil)));; (* *)
+(* the former  is read right  to left, while the latter, left to right *)
+let elements_list_of_lists l =  my_fold (+) 0 (my_map my_len l)
+let elements_list_of_lists_pipeline l =  l |> my_map my_len |> my_fold (+) 0
---
+++ sections/sec01/ex1_holes.ml
@@ -0,0 +1,37 @@
+let hole () = failwith "hole";;
+
+(* 'a is a "type variable" - which means it's a "placeholder" for a type, that can be substituted in function application *)
+(* what is the type of (id 3)? What is the type of (id "hello world")?  How do we figure that out? *)
+let id (x : 'a) : 'a = x;;
+
+(* recursive data type - a list *)
+(* You haven't learned ADTs yet in lecture, but think of this as a "preview" for the next lecture *)
+type 'a mylist = MNil | MCons of 'a * 'a mylist;;
+let example_list : int mylist = (MCons (3 ,(MCons (1, (MCons (2, MNil))))));;
+
+(* length function *)
+let rec my_len: 'a mylist -> int = hole;;
+
+Printf.printf "Length of example_list is: %d" (my_len example_list);;
+
+(* buggy - actual bug as I was writing this. Type signature will give away the bug.. *)
+(* use trace to fix! *)
+let rec buggy_my_len = hole;;
+Printf.printf "Length of example_list with buggy function is: %d" (buggy_my_len example_list);;
+(* if we have time, try to define these. Otherwise, *)
+
+(* map - given a function and a list, return a new list with the function to applied to all elements of the given list *)
+(* another way to write functions *)
+let rec my_map (f: ('a -> 'a)) (l: 'a mylist) : 'a mylist = hole;;
+(* example usage: my_map (fun x -> x + 3) example_list;; *)
+
+(* right associative fold - simpler, but more restrictive, type *)
+let rec my_fold (f: ('a -> 'a -> 'a)) (i: 'a) (l: 'a mylist ): 'a = hole;;
+(* example usage: my_fold (fun a b -> a + b) 0 example_list;; *)
+
+
+(* composition! Count elements in a list of lists! *)
+let example_list_of_lists: int mylist mylist = (MCons (example_list, MCons (example_list, MNil)));; (* *)
+
+(* how do we use the functions we've already defined to solve this problem? *)
+let elements_list_of_lists (l : 'a mylist mylist) = hole;;
